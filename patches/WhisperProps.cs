b716926a027f983a4c0e024e84512dbd042d95e9
FLSoz <65517812+FLSoz@users.noreply.github.com>
2022-04-08 19:48:11 -0700

    Added Whisper Props from https://github.com/LegioniteTerraTech/WhisperProps

---
+++ patches/WhisperProps.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using System.Reflection;
+
+namespace CommunityPatch.patches
+{
+    internal class WhisperProps
+    {
+        // Sourced from Legionite with permission: https://github.com/LegioniteTerraTech/WhisperProps
+        [HarmonyPatch(typeof(ModuleBooster))]
+        [HarmonyPatch("OnPool")]
+        private class ShhhhhhushMyChild
+        {
+            private static void Postfix(ModuleBooster __instance)
+            {
+                FieldInfo lockGet = typeof(ModuleBooster).GetField("m_BoosterAudioType", BindingFlags.NonPublic | BindingFlags.Instance);
+                int m_BoosterAudioType = (int)lockGet.GetValue(__instance);
+                if (m_BoosterAudioType == (int)TechAudio.BoosterEngineType.Propeller)
+                {
+                    lockGet.SetValue(__instance, (int)TechAudio.BoosterEngineType.MediumRotor);
+                }
+            }
+        }
+    }
+}
